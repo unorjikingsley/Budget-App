226bdb95f70f2b918bfeaa07f66a24a5dc5ba0e9
unorji kingsley <unorjikingsley@gmail.com>
2023-09-08 21:35:00 +0100

    uncomment

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :entities, dependent: :destroy, foreign_key: 'author_id'
   has_many :groups, dependent: :destroy, # foreign_key: 'author_id'
 
-  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # devise_scope :user do
-  #   unauthenticated :user do
-  #     root to: 'devise/sessions#splash', as: :unauthenticated_root
-  #   end
-  # end
+  devise_scope :user do
+    unauthenticated :user do
+      root to: 'devise/sessions#splash', as: :unauthenticated_root
+    end
+  end
 end
