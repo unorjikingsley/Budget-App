718fb44124a87245787c6231615b6162a69eb638
unorji kingsley <unorjikingsley@gmail.com>
2023-09-12 18:09:22 +0100

    Solve linters

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,9 +3,6 @@ class Group < ApplicationRecord
   has_and_belongs_to_many :entities
   has_one_attached :icon
 
-  # has_many :entities_group_to_joins, dependent: :destroy
-  # has_many :entities, through: :entities_group_to_joins
-
   validates :name, presence: true
   validates :icon, presence: true
 end
